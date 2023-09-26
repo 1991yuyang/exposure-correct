0c12665663ed420fd50188bf98809e103477f184
yuyang <1230-jj@163.com>
2023-09-26 14:25:23 +0800

    add convert to onnx

---
+++ convert_2_onnx.py
@@ -0,0 +1,40 @@
+import onnx
+import onnxruntime
+import torch.onnx
+from model_def import ECNet
+import json
+import torch as t
+from tools import generate_laplacian_pyram
+import numpy as np
+from numpy import random as rd
+
+
+onnx_params = json.load(open("conf.json", "r", encoding="utf-8"))["convert_to_onnx"]
+laplacian_level_count = onnx_params["laplacian_level_count"]
+layer_count_of_every_unet = onnx_params["layer_count_of_every_unet"]
+first_layer_out_channels_of_every_unet = onnx_params["first_layer_out_channels_of_every_unet"]
+pth_file_path = onnx_params["pth_file_path"]
+use_iaff = onnx_params["use_iaff"]
+dummy_input_image_size = onnx_params["dummy_input_image_size"]
+onnx_output_path = onnx_params["onnx_output_path"]
+input_names = ["in%d" % (i,) for i in range(1, 1 + laplacian_level_count)]
+output_names = ["out%d" % (i,) for i in range(1, 1 + laplacian_level_count)]
+iaff_r = onnx_params["iaff_r"]
+
+
+dummy_inputs = rd.randint(0, 255, dummy_input_image_size + [3]).astype(np.uint8)
+dummy_inputs = [t.from_numpy(np.expand_dims(np.transpose(i, axes=[2, 0, 1]), axis=0).astype(np.float32)).type(t.FloatTensor) for i in generate_laplacian_pyram(dummy_inputs, laplacian_level_count)[0]]
+
+
+model = ECNet(laplacian_level_count, layer_count_of_every_unet, first_layer_out_channels_of_every_unet, use_iaff, iaff_r)
+model.load_state_dict(t.load(pth_file_path))
+model.eval()
+
+
+names = input_names + output_names
+dynamic_axes = {name: {0: "batch", 2: "height", 3: "width"} for name in names}
+print("input_names:", input_names)
+print("output_names:", output_names)
+print("dynamic_axes:", dynamic_axes)
+
+torch.onnx.export(model, dummy_inputs, onnx_output_path, input_names=input_names, output_names=output_names, opset_version=12, dynamic_axes=dynamic_axes)
